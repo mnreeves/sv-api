3c14d5cef7970f8faf1a924559b76be5671710e3
mnreeves <natsir.productions@gmail.com>
2022-12-01 19:49:43 +0700

    parsing payload

--- src/utils/bootstrap_express.ts
+++ src/utils/bootstrap_express.ts
@@ -6,6 +6,8 @@ const app = Express();
 const port = Config.applicationPort;
 
 export default function BootstrapExpress() {
+  app.use(Express.json());
+  app.use(Express.urlencoded({ extended: false }));
   app.use('', routerApp);
   app.listen(port, () => {
     console.log(`application running at port: ${port}`);
